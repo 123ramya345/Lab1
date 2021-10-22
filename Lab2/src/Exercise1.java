b408f19ff3ef54214ed658c43ff1c80a28948de6
Ramya Ellandula <92844252+123ramya345@users.noreply.github.com>
2021-10-22 17:48:04 +0530

    Update Exercise1.java

--- Lab2/src/Exercise1.java
+++ Lab2/src/Exercise1.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class Exercise1 {
   public static void main(String[] args) {
-    // Intialising the variables
+   
     int n, min;
     Scanner Sc = new Scanner(System.in);
 
-    // Enter the number of elements.
+   
     System.out.print("Enter number of elements : ");
     n = Sc.nextInt();
 
-    // creating an array.
+   
     int a[] = new int[n];
 
-    // enter array elements.
+    
     System.out.println("Enter the elements in array : ");
     for (int i = 0; i < n; i++) {
       a[i] = Sc.nextInt();
